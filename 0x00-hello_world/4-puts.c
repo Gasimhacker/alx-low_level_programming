7109d7ce6e0454488f9b6eb061981f9f843eae6a
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-13 20:02:12 +0000

    Print using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This function prints "Programming is like building a multilingual puzzle"
+ * return:0 succeed.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
