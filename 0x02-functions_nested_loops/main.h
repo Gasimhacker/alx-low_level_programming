0a862e228a6a7bfa453c5049204e3d88f355788d
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-18 19:30:49 +0000

    Print the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,9 @@
  * Return: On success 1.
  */
 int _putchar(char c);
+
+/*
+ * print_alphabet - Print the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 .
+ */
+void print_alphabet(void);
