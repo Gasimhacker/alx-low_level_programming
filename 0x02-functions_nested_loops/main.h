e4b0d3a42843e14680dfad8653ac534ed05cfe0a
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-18 22:33:10 +0000

    Add two integers and return the result

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -66,3 +66,12 @@ void jack_bauer(void);
  * Return: Always void.
  */
 void times_table(void);
+
+/**
+ * add - Add two integers
+ * @n1: The number to add another number to it
+ * @n2: The number added to the first number
+ *
+ * Return: The result of the addition.
+ */
+int add(int n1, int n2);
