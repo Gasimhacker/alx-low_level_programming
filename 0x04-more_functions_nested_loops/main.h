fbfe809c83685b89a226da9a66989e076963d729
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-20 19:36:19 +0000

    Print 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -45,4 +45,11 @@ void print_numbers(void);
  */
 void print_most_numbers(void);
 
+/**
+ * more_numbers - Print 10 times the numbers,
+ *		  from 0 to 14, followed by a new line
+ * Return: Always void.
+ */
+void more_numbers(void);
+
 #endif
