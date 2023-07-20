fcfbfb6b481bdc92ab87da03e73770a55dfa9352
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-20 18:44:19 +0000

    Check for uppercase character

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This part is about learning more on functions and nested loops
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+
+/**
+ * _isupper - Check for uppercase character.
+ * @c: The character to check
+ * Return: 1 if c is uppercase 0 otherwise.
+ */
+int _isupper(int c);
+
+#endif
