bec0d6192395a65a89051324eddf851b47181b02
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-20 18:49:45 +0000

    Check for a digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,4 +15,11 @@ int _putchar(char c);
  */
 int _isupper(int c);
 
+/**
+ * _isdigit - Check for a digit.
+ * @c: The character to check
+ * Return: 1 if c is digit 0 otherwise.
+ */
+int _isdigit(int c);
+
 #endif
