e8cfe5a83de94e80b6bd3437ae6a881f98e196c0
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-20 19:01:10 +0000

    Print the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -31,4 +31,10 @@ int _isdigit(int c);
  */
 int mul(int a, int b);
 
+/**
+ * print_numbers - Print the numbers, from 0 to 9, followed by a new line
+ * Return: Always void.
+ */
+void print_numbers(void);
+
 #endif
