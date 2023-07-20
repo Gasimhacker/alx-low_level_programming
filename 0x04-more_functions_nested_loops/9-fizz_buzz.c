e4808f57331cc7271902cad401f7f794343f54a6
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-20 22:03:30 +0000

    Remove the extra space at the end of the last number

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,23 @@ int main(void)
 	{
 		if (count % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (count % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (count % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", count);
+			printf("%d", count);
+		}
+		if (count != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
