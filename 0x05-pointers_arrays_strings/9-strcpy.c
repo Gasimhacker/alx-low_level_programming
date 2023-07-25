b24ea991aef76f6dd89a8b88b083971603e7a3c1
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-25 01:02:03 +0000

    Add the null terminator

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *ptr = dest
 
-	while (src[index])
+	while (*src != '\0')
 	{
-		dest[index] = src[index];
-		index++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
+	*dest = '\0';
 
-	dest[index + 1 ] = '\0';
-
-	return (dest);	
+	return (ptr);
 }
