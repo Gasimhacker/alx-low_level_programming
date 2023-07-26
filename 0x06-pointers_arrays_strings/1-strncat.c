4e844ccd293ff01fec7c4c2bf5c52e0da34c0ce0
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-26 06:43:40 +0000

    Concatenate n characters from src to dest

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
-#include "main.h"
 /**
- * _strcat - Concatenate two strings
+ * _strncat - Concatenate two strings
  * @src: The string to be appended
  * @dest: The string to append to it
+ * @n: The number of the concatenated chracters
  *
  * Return: The pointer to the destination.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	char *ptr = dest;
 
@@ -14,10 +14,12 @@ char *_strcat(char *dest, char *src)
 	{
 		dest++;
 	}
-	while ((*src) != '\0')
+
+	while ((*src) != '\0' && n--)
 	{
 		*dest++ = *src++;
 	}
+
 	*dest = '\0';
 
 	return (ptr);
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,14 @@
  */
 char *_strcat(char *dest, char *src);
 
+/**
+ * _strncat - Concatenate two strings
+ * @src: The string to be appended
+ * @dest: The string to append to it
+ * @n: The number of the concatenated chracters
+ *
+ * Return: The pointer to the destination.
+ */
+char *_strncat(char *dest, char *src, int n);
 
 #endif
