b9e6140e18709d1ddc451bb3f9f6850078e1c190
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-31 18:10:44 +0000

    Fill memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This part contains more advanced tasks about pointers
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+
+/**
+ * memset - Fill memory with a constant byte
+ * @s: A pointer to the memory area to be filled
+ * @b: The constant byte to fill with
+ * @n: The number of bytes to be filled
+ *
+ * Return: A pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
