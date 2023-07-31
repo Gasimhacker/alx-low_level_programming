e46268fd4427e0ba5341da44b907fae69bb70659
Mohammed Ahmed <ma2286572@gmail.com>
2023-07-31 21:36:55 +0000

    Locate a substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -62,5 +62,15 @@ unsigned int _strspn(char *s, char *accept);
  */
 char *_strpbrk(char *s, char *accept);
 
+/**
+ * _strstr - Locate a substring
+ * @haystack: The string to be searched
+ * @needle: The substring that should be located
+ *
+ * Return: A pointer to the beginning of the located
+ *	   substring, or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle);
+
 
 #endif
