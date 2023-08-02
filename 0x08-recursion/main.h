58dd11d08ae74f7eed5a0caa3f2bddb8fb151ee1
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-02 08:43:40 +0000

    Print a string, followed by a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - Print  a string, followed by a new line
+ * @s: The string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s);
+
+#endif
