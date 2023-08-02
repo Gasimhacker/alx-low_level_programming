83a6f7a93ad2298aa7f314cb1a79e272d3bc4066
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-02 19:56:38 +0000

    Check if the input integer is a prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -60,5 +60,13 @@ int _pow_recursion(int x, int y);
  */
 int _sqrt_recursion(int n);
 
+/**
+ * is_prime_number - Check if the number is prime
+ * @n: The number to be checked
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n);
+
 
 #endif
