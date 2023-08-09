5034c8dc19faf1e6a0527791b5989b985e266792
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-09 03:09:51 +0000

    Fix output

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -48,18 +48,21 @@ char **strtow(char *str)
 
 	for (i = 0; i <= len; i++)
 	{
-		if ((str[i] == ' ' || str[i] == '\0') && c)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			end = i;
-			tmp = (char *) malloc(sizeof(char) * (c + 1));
-			if (tmp == NULL)
-				return (NULL);
-			while (start < end)
-				*tmp++ = str[start++];
-			*tmp = '\0';
-			matrix[k] = tmp - c;
-			k++;
-			c = 0;
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
 		}
 		else if (c++ == 0)
 			start = i;
