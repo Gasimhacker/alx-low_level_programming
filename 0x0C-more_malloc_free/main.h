bb5a374c27ab03dc682467aadc66fd1036c03b56
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-10 18:55:00 +0000

    Write a function that concatenates two strings taking n characters from the second string

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,19 @@
  */
 void *malloc_checked(unsigned int b);
 
+/**
+ * string_nconcat - Concatenate two strings given as a parameter into
+ *		    a newly allocated memory space taking n characters
+ *		    from the second string
+ * @s1: The string to be put first in the new string
+ * @s2: The string to be put second in the new string
+ * @n: The number of characters to be taken
+ *     from the second string, if n is greater than
+ *     or equal the length of s2 then use the entire string s2
+ *
+ * Return: A pointer to the concatenated string, NULL otherwise
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 
 #endif
