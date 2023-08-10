c5c65d8fb657ce51eb2498a17b988559cb66be4b
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-10 23:08:47 +0000

    Write a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -51,6 +51,26 @@ void *_calloc(unsigned int nmemb, unsigned int size);
  */
 int *array_range(int min, int max);
 
+/**
+ * _realloc - Reallocate memory block using malloc and free
+ *	      and copy the content of ptr to the newly allocated space,
+ *	      in the range from the start of ptr up to
+ *	      the minimum of the old and new sizes.
+ *	      If new_size > old_size, the "added" memory
+ *	      should not be initialized
+ * @ptr: A pointer to the memory previously allocated with a call to malloc:
+ *	 "malloc(old_size)"
+ * @old_size: The size, in bytes, of the allocated space for ptr
+ * @new_size: The size, in bytes, of the new memory block
+ *
+ * Return: A pointer to the allocated memory,
+ *	   if new_size == old_size,do not do anything and return ptr
+ *	   If new_size is equal to zero, and ptr is not NULL,
+ *	   then the call is equivalent to free(ptr). Return NULL
+ *	   If malloc fails, then _realloc returns NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 
 
 #endif
