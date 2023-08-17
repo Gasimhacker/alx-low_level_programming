b534da838edbcfe7b83dff82fc94762b295db883
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-17 06:00:46 +0000

    Write a function that returns the sum of all its parameters

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+/**
+ * sum_them_all - Sum all of the passed parameters
+ * @n: The number of parameters that will be passed after n
+ *
+ * Return: The sum if n > 0, otherwise 0
+ */
+int sum_them_all(const unsigned int n, ...);
+
+#endif
