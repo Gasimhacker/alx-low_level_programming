e772a6e0302bed5b27eb2783b1c90ce5ecd92e5a
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-17 21:45:11 +0000

    Print (nil) instead of nill

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,10 +20,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(p, char *);
 
-		if (str == NULL)
-			printf("nil");
-		else
+		if (str)
 			printf("%s", str);
+		else
+			printf("(nil)");
 
 		if (separator && (i != n - 1))
 			printf("%s", separator);
