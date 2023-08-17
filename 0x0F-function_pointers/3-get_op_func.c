f8e873037b76c5ad702d35fa2960c411cccd6782
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-17 01:52:53 +0000

    Make sure that the operator passed contains one chatacter only

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,7 +25,7 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && (*(s + 1) == '\0'))
 	{
 		if (*s == *(ops[i].op))
 			return (ops[i].f);
