49f8161f580ece4e92d4a79b9df956d6535ec648
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-24 21:33:52 +0000

    Write a function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This part is about learning how to use singly linked lists
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,39 @@
+#ifndef LIST_H
+#define LIST_H
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+/**
+ * print_list - Print the data stored in the nodes of a linked list
+ * @h: A pointer to the first node in the linked list
+ *
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h);
+
+#endif /* LIST_H */
