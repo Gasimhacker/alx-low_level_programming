719fee3a0ffd46b02abaa58606a0d8303b430eef
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-25 01:33:28 +0000

    Write a 64-bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+	global main
+	extern printf
+
+
+	section  .text
+main:
+	lea rdi, [rel msg]	;	The address of the string to print
+	mov al, 0		;	no SSE regs used
+	call	printf    	;	"Hello, Holberton\n"
+
+	mov rax, 0x3c		;	exit
+	xor rdi, rdi		;	EXIT_SUCCESS
+	syscall			;	exit(EXIT_SUCCESS)
+
+
+	section  .data
+
+	msg: db "Hello, Holberton", 10
