a67debbcf6dcd886f299bde93de6b0bc683c1704
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-25 01:57:15 +0000

    Try to return zero without push and pop

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,14 +4,11 @@
 
 	section  .text
 main:
-	push rbp
-
 
 	mov rdi, msg		;	The address of the string to print
 	mov rax, 0		;	no SSE regs used
 	call	printf    	;	"Hello, Holberton\n"
 
-	pop rbp
 
 	mov rax, 0		;
 	ret			;	return (0)
