89283bb2abad1c9f716e2aa00ce658b4e0534b4b
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-28 03:58:27 +0000

    Write a function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,4 +27,12 @@ typedef struct listint_s
  */
 size_t print_listint(const listint_t *h);
 
+/**
+ * listint_len - Return the number of elemnts in listint_t list
+ * @h: A pointer to the head of the linked list
+ *
+ * Return: The number of nodes in the linked list
+ */
+size_t listint_len(const listint_t *h);
+
 #endif /* LISTS_H */
