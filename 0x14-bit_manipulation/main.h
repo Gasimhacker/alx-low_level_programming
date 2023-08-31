6ed00baf8ba2656565aff20d0ab84c615b82e4c9
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-31 16:56:21 +0000

    Create a function that sets the value of a bit to 0 at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -47,4 +47,13 @@ int get_bit(unsigned long int n, unsigned int index);
  */
 int set_bit(unsigned long int *n, unsigned int index);
 
+/**
+ * clear_bit - Set the value of a bit at a given index to 0
+ * @n: The number to clear the value of its bit at a given index
+ * @index: The index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index);
+
 #endif /* MAIN_H */
