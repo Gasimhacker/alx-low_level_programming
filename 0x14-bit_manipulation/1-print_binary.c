983fd5247f04b3bc28a778c8787ed858922c1e23
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-31 14:30:59 +0000

    Create a function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * print_binary - Print the binary representation of a number
+ * @n: The number to print its binary representation
+ *
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i, start_flag = 0;
+
+	if (n == 0 || n == 1)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	for (i = 1 << 31; i > 0; i >>= 1)
+	{
+		if (n & i)
+		{
+			start_flag = 1;
+			_putchar('1');
+		}
+		else if (start_flag)
+			_putchar('0');
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,15 @@
 
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * binary_to_uint - Convert a binary number to an unsigned int
  * @b: A pointer to a string of 0 and 1 chars that should be converted
  *
@@ -11,5 +20,13 @@
  *	   b is NULL
  */
 unsigned int binary_to_uint(const char *b);
+
+/**
+ * print_binary - Print the binary representation of a number
+ * @n: The number to print its binary representation
+ *
+ * Return: Void
+ */
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
