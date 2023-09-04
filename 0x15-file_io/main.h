cd8642251cf7b2f28c6f1978889b7c23ca3a5d54
Mohammed Ahmed <ma2286572@gmail.com>
2023-09-04 08:25:23 +0000

    Write a function that appends text at the end of a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -29,4 +29,16 @@ ssize_t read_textfile(const char *filename, size_t letters);
  */
 int create_file(const char *filename, char *text_content);
 
+/**
+ * append_text_to_file - Append a text at the end of a file
+ * @filename: The name of the file
+ * @text_content: A NULL terminated string to add at the end of the file
+ *
+ * Return: 1 - On success
+ *	  -1 - On failure (If filename is NULL,If text_content is NULL
+ *	  and the file does not exist or if you do not have the
+ *	  required permissions to write the file)
+ */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif /* MAIN_H */
