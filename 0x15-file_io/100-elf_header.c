f609ff54b5776fe05a062b699e8b98bf5b332561
Mohammed Ahmed <ma2286572@gmail.com>
2023-09-05 13:03:15 +0000

    Delete the extra printed space

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -58,8 +58,14 @@ void print_magic(unsigned char *e_ident)
 
 	printf("  Magic:   ");
 	for (index = 0; index < EI_NIDENT; index++)
-		printf("%02hx ", e_ident[index]);
-	printf("\n");
+	{
+		printf("%02x", e_ident[index]);
+
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else 
+			printf(" ");
+	}
 }
 
 
