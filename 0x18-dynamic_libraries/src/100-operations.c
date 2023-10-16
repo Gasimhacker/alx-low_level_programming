185272c43135a5d6c1e7a2838f2f46d9e9637eff
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-16 21:49:07 +0000

    Create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/src/100-operations.c
@@ -0,0 +1,60 @@
+
+/**
+ * add - Add 2 numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtract 2 numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiply 2 numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divide 2 numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Calculate the modules of the division between 2 numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
