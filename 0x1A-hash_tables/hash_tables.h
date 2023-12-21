803e162b0fdfb571648d373a336363b496731f6a
Mohammed Ahmed <ma2286572@gmail.com>
2023-12-21 23:43:22 +0000

    Write a hash function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -55,4 +55,15 @@ hash_table_t *hash_table_create(unsigned long int size);
  */
 unsigned long int hash_djb2(const unsigned char *str);
 
+/**
+ * key_index - Find the index according to the given key
+ * @key: The key
+ * @size: The size of the array of the hash table
+ *
+ * Return: The index at which the key/value pair
+ *	   should be stored in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+
 #endif /* HASH_TABLE_H */
