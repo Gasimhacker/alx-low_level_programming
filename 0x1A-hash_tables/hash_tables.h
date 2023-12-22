58f84ead3b54a1c8427254b254a5ffc85928c413
Mohammed Ahmed <ma2286572@gmail.com>
2023-12-22 03:03:32 +0000

    Write a function that deletes a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -94,5 +94,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
  */
 void hash_table_print(const hash_table_t *ht);
 
+/**
+ * hash_table_delete - Delete a hash table
+ * @ht: A pointer to the hash table
+ *
+ * Return: Void
+ */
+void hash_table_delete(hash_table_t *ht);
+
 
 #endif /* HASH_TABLE_H */
