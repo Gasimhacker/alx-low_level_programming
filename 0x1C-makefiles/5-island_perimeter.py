bd4e403347b50cf7191e00c18cd83b590cccae8d
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-22 01:50:16 +0000

    Solve the indexError and the pycode style error

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,7 @@
 
 """A module that Calculates the perimeter of an island"""
 
+
 def island_perimeter(grid):
     """Calculate the perimeter of the island described in grid
 
@@ -23,12 +24,12 @@ def island_perimeter(grid):
         The perimeter of the island
     """
     perimeter = 0
-    for i in range (len(grid)):
+    for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
-                perimeter += (4
-                              - grid[i][j + 1]
-                              - grid[i][j - 1]
-                              - grid[i + 1][j]
-                              - grid[i - 1][j])
+                perimeter += 4
+                if (j > 0 and grid[i][j - 1] == 1):
+                    perimeter -= 2
+                if (i > 0 and grid[i - 1][j] == 1):
+                    perimeter -= 2
     return perimeter
