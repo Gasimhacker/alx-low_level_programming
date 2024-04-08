0f293b1efe9d20e47606b9f4e9d97643681299ca
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-08 05:54:16 +0000

    Implement the binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -16,5 +16,15 @@
  *	   Otherwise - "-1"
  */
 int linear_search(int *array, size_t size, int value);
+/**
+ * binary_search - The binary search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: If the value is pesent - The index where value is located
+ *	   Otherwise - "-1"
+ */
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
