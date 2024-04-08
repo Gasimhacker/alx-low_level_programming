5cd6f82ff21d974df4d04673c9381d3531a5a7fc
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-08 05:25:05 +0000

    Implement the linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,20 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* "Header files" */
+#include <stdio.h>
+
+
+/* "Functions" */
+/**
+ * linear_search - The linear search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: If the value is pesent - The index where value is located
+ *	   Otherwise - "-1"
+ */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
